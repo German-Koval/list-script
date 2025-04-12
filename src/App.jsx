edc75b23ace5a55d26c564b3bedaa7d636330f24
v.cherednichenko <vcherednichenko@icloud.com>
2025-04-12 08:55:20 +0300

    feature: добавлена кнопка копирования и подкорректирован UI/UX

--- src/App.jsx
+++ src/App.jsx
@@ -4,45 +4,92 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-
-  const [groupId, setGroupId] = useState(false);
+  const [groupId, setGroupId] = useState('');
   const [modList, setModList] = useState(null);
+  const [showGroupLink, setShowGroupLink] = useState(false);
+  const [copyAnimation, setCopyAnimation] = useState(false);
+  const [buttonText, setButtonText] = useState('Копи');
+
+  const headerText = 'Отправляю список учеников, которые еще не подключились к уроку. Мы вас ждем:';
+  const groupLink = `https://backoffice.algoritmika.org/group/view/${groupId}`;
+
   function updateValue(e) {
     e.preventDefault()
     const formData = new FormData(e.target)
     const formJson = Object.fromEntries(formData.entries());
     console.log(formJson)
-    console.log(formJson.studList.split('\n').join(' '))
+-   console.log(formJson.studList.split('\n').join(' '))
     listTransform(formJson.studList.split('\n').join(' '))
     setGroupId(formJson.groupId)
   }
+
   function listTransform(oldList) {
     setModList(oldList.split('трек').map((el)=>{ return el.split(' ').filter((el) => el != '' && isNaN(el)).slice(0,2).join(' ')})) 
   }
- function deleteStudent(ind) {
-  setModList(modList.toSpliced(ind,1))
- }
 
+  function deleteStudent(ind) {
+    setModList(modList.toSpliced(ind,1))
+  }
 
-  const resultingList = (modList === null) ? false : modList.map((el,ind) => {return <div><span className="result__list-el"onClick={() => deleteStudent(ind)}>{el}</span></div>}) ;
- 
+  function copyToClipboard() {
+    if (!modList) return;
+    
+    let textToCopy = `${headerText}\n\n`;
+    
+    if (showGroupLink && groupId) {
+      textToCopy += `${groupLink}\n\n`;
+    }
+    
+    textToCopy += modList.join('\n');
+    
+    navigator.clipboard.writeText(textToCopy);
+    setCopyAnimation(true);
+    setButtonText('Мэч');
+    
+    setTimeout(() => {
+      setCopyAnimation(false);
+      setButtonText('Копи');
+    }, 2000);
+  }
+
+  const resultingList = (modList === null) 
+    ? false 
+    : modList.map((el,ind) => {return <div><span className="result__list-el"onClick={() => deleteStudent(ind)}>{el}</span></div>}) ;
  
   return (
     <>
       <h1>Спискоделатель</h1>
       <div className='main-container'>
         <div className='form-container'>
-          <form  onSubmit={updateValue} >
+          <form onSubmit={updateValue}>
             <textarea name="studList" placeholder='Cкопируй сюда список из вкладки "ученики"' ></textarea>
-            <input name="groupId" placeholder='id группы'></input>
-            <button type="submit" >Тыц</button>
+            <div className="input-group">
+              <input 
+                name="groupId" 
+                placeholder='id группы'
+                value={groupId}
+                onChange={(e) => setGroupId(e.target.value)}
+              />
+              <label>
+                <input type="checkbox" onChange={(e) => setShowGroupLink(e.target.checked)} />
+                Показать ссылку
+              </label>
+            </div>
+            <button type="submit">Тыц</button>
           </form>
         </div>
         <div className='result-container'>
-        <div className='group-link'>https://backoffice.algoritmika.org/group/view/{groupId}</div>
-        <h4>Отправляю список ребят, которые еще не подключились к уроку. Мы вас ждем: 
-        </h4>
-        {resultingList}
+          {modList && (
+            <button 
+              className={`copy-button ${copyAnimation ? 'copy-animation' : ''}`}
+              onClick={copyToClipboard}
+            >
+              {buttonText}
+            </button>
+          )}
+          {showGroupLink && groupId && <div className='group-link'>{groupLink}</div>}
+          <h4>{headerText}</h4>
+          {resultingList}
         </div>
       </div>
     </>
